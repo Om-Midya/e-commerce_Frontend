d004187fcf783c1a507c41a7398b87f1d2c52325
Om-Midya <dukemidya7521@gmail.com>
2024-06-27 18:35:33 +0530

    Added search bar for the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,15 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
 import Avatar from '@mui/material/Avatar';
-import { FaShoppingCart, FaUser, FaBars } from 'react-icons/fa';
+import { FaShoppingCart, FaUser, FaBars, FaSearch } from 'react-icons/fa';
+import {InputAdornment} from "@mui/material";
+import {AccountCircle} from "@mui/icons-material";
 
 export const Header: React.FC = () => {
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [search, setSearch] = React.useState<string>('');
 
     const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorEl(event.currentTarget);
@@ -20,12 +24,26 @@ export const Header: React.FC = () => {
         setAnchorEl(null);
     };
 
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(event.target.value);
+    };
+
+    const handleSearch = () => {
+        console.log('Search:', search);
+        // Implement your search logic here
+    };
+
     return (
         <AppBar position="fixed">
-            <Toolbar>
+            <Toolbar
+                style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                }}
+            >
                 <Typography variant="h6" style={{
                     display: 'flex',
-                    flexGrow: 1,
+                    justifyContent: 'space-around',
                 }}>
                     E-Commerce
                 </Typography>
@@ -40,26 +58,41 @@ export const Header: React.FC = () => {
                         <MenuItem onClick={handleClose}>Logout</MenuItem>
                     </Menu>
                 </div>
-                <div style={{
-                    width: '20%',
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                }}>
-                    <Button color="inherit" startIcon={<FaShoppingCart />}>
-                        Cart
-                    </Button>
-                    <Button color="inherit" startIcon={<FaUser />}>
-                        Login
-                    </Button>
-                    <IconButton
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        onClick={handleMenu}
-                    >
-                        <FaBars />
-                    </IconButton>
-                </div>
+
+                <TextField
+                    id="input-with-icon-adornment"
+                    value={search}
+                    onChange={handleSearchChange}
+                    placeholder="Search..."
+                    variant="outlined"
+                    size="small"
+                    style={{
+                        marginRight: '10px',
+                        width: '60%',
+                        backgroundColor: 'white',
+                }}
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <FaSearch />
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <Button color="inherit" startIcon={<FaShoppingCart />}>
+                    Cart
+                </Button>
+                <Button color="inherit" startIcon={<FaUser />}>
+                    Login
+                </Button>
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    onClick={handleMenu}
+                >
+                    <FaBars />
+                </IconButton>
 
             </Toolbar>
         </AppBar>
